97baaee49176837af526cff95c1d1cc6761587fa
jenni <jennifergds89@gmail.com>
2023-05-02 14:51:22 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("*** rolando dado ***");
+
+Console.WriteLine();
+
+Console.Write("quantos lados? ");
+
+int lados = Convert.ToInt32(Console.ReadLine());
+int rolagem = RandomNumberGenerator.GetInt32(1, lados + 1);
+
+Console.WriteLine($"\nRolagem: {rolagem}");
